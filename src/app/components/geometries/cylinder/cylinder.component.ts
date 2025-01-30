57c2379bb8e93b2dec57da00a09f6e4e6e0ea662
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-01-30 17:38:09 -0600

    Add Geometries Components , Service , and Model

---
+++ src/app/components/geometries/cylinder/cylinder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cylinder',
+  imports: [],
+  templateUrl: './cylinder.component.html',
+  styleUrl: './cylinder.component.css'
+})
+export class CylinderComponent {
+
+}
---
+++ src/app/components/geometries/sphere/sphere.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sphere',
+  imports: [],
+  templateUrl: './sphere.component.html',
+  styleUrl: './sphere.component.css'
+})
+export class SphereComponent {
+
+}
